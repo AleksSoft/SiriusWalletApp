aa285edc8fd0321cf7d7dc33d90cb4fac792445f
Antoraro <anton.rozdorozhnuk@gmail.com>
2020-06-01 10:26:47 +0300

    Widndows app name and resolution fixed

--- windows/runner/window_configuration.cpp
+++ windows/runner/window_configuration.cpp
@@ -1,7 +1,7 @@
 #include "window_configuration.h"
 
-const wchar_t* kFlutterWindowTitle = L"SiriusWalletApp";
+const wchar_t* kFlutterWindowTitle = L"Antares Wallet";
 const unsigned int kFlutterWindowOriginX = 10;
 const unsigned int kFlutterWindowOriginY = 10;
-const unsigned int kFlutterWindowWidth = 1280;
-const unsigned int kFlutterWindowHeight = 720;
+const unsigned int kFlutterWindowWidth = 480;
+const unsigned int kFlutterWindowHeight = 800;
