dd8f6d79a0c2c1d6caec1c0179824eb3a39ca6f8
Antoraro <anton.rozdorozhnuk@gmail.com>
2020-06-29 14:44:57 +0300

    Pair trading view done

--- lib/ui/views/pair_trading/pair_trading_view.dart
+++ lib/ui/views/pair_trading/pair_trading_view.dart
@@ -3,6 +3,8 @@ import 'package:antares_wallet/models/market_model.dart';
 import 'package:antares_wallet/ui/common/app_colors.dart';
 import 'package:antares_wallet/ui/common/app_sizes.dart';
 import 'package:antares_wallet/ui/common/app_ui_helpers.dart';
+import 'package:antares_wallet/ui/widgets/volume_ask_tile.dart';
+import 'package:antares_wallet/ui/widgets/volume_bid_tile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -49,15 +51,33 @@ class PairTradingView extends StatelessWidget {
             ),
           ),
           body: ListView(
+            shrinkWrap: true,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(AppSizes.medium),
-                child: _HeaderView(),
-              ),
+              _HeaderView(),
               Divider(height: 1),
               _CandleChartView(data: model.mockMarkets),
               Divider(height: 1),
-              AppUiHelpers.vSpaceExtraLarge,
+              Container(
+                height: 27 * 40.0,
+                child: DefaultTabController(
+                  initialIndex: 0,
+                  length: 2,
+                  child: Column(
+                    children: [
+                      TabBar(
+                        indicatorWeight: 1.0,
+                        indicatorColor: Colors.black,
+                        tabs: [Tab(text: 'Order book'), Tab(text: 'Tradelog')],
+                      ),
+                      Expanded(
+                        child: TabBarView(
+                          children: [_Orderbook(), _Tradelog()],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         );
@@ -75,56 +95,59 @@ class _HeaderView extends StatelessWidget {
           fontFamily: 'Akrobat',
           fontWeight: FontWeight.w700,
         );
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text('USD 0,00', style: titleTheme),
-        AppUiHelpers.vSpaceExtraSmall,
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      '-3,44%',
-                      style: Theme.of(context).textTheme.button.copyWith(
-                            color: AppColors.red,
-                          ),
-                    ),
-                    Icon(Icons.arrow_drop_down, color: AppColors.red),
-                  ],
-                ),
-                AppUiHelpers.vSpaceExtraSmall,
-                Text(
-                  'Vol 3,3327',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-              ],
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  'High 9 431, 97',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                AppUiHelpers.vSpaceExtraSmall,
-                Text(
-                  'Low 8 950, 34',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-              ],
-            ),
-          ],
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(AppSizes.medium),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text('USD 0,00', style: titleTheme),
+          AppUiHelpers.vSpaceExtraSmall,
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        '-3,44%',
+                        style: Theme.of(context).textTheme.button.copyWith(
+                              color: AppColors.red,
+                            ),
+                      ),
+                      Icon(Icons.arrow_drop_down, color: AppColors.red),
+                    ],
+                  ),
+                  AppUiHelpers.vSpaceExtraSmall,
+                  Text(
+                    'Vol 3,3327',
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    'High 9 431, 97',
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                  AppUiHelpers.vSpaceExtraSmall,
+                  Text(
+                    'Low 8 950, 34',
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
@@ -143,26 +166,28 @@ class _CandleChartView extends StatelessWidget {
           zoomPanBehavior: ZoomPanBehavior(
             enablePinching: true,
             enablePanning: true,
+            zoomMode: ZoomMode.x,
           ),
           plotAreaBorderWidth: 0,
           primaryXAxis: DateTimeAxis(
-            dateFormat: DateFormat.MMM(),
-            interval: 3,
-            intervalType: DateTimeIntervalType.months,
+            enableAutoIntervalOnZooming: true,
+            dateFormat: DateFormat.MMMd(),
+            intervalType: DateTimeIntervalType.days,
             majorGridLines: MajorGridLines(width: 0),
           ),
           primaryYAxis: NumericAxis(
-            labelFormat: '\${value}',
+            interval: 50,
+            opposedPosition: true,
+            labelStyle: ChartTextStyle(color: AppColors.secondary),
             axisLine: AxisLine(width: 0),
           ),
           series: <CandleSeries<MarketModel, DateTime>>[
             CandleSeries<MarketModel, DateTime>(
+              dataSource: data,
               enableTooltip: true,
               enableSolidCandles: true,
-              dataSource: data,
-              name: 'AAPL',
               xValueMapper: (MarketModel sales, _) =>
-                  DateTime.fromMillisecondsSinceEpoch(sales.date),
+                  DateTime.fromMillisecondsSinceEpoch(sales.date * 1000),
               lowValueMapper: (MarketModel sales, _) => sales.low,
               highValueMapper: (MarketModel sales, _) => sales.high,
               openValueMapper: (MarketModel sales, _) => sales.open,
@@ -176,5 +201,123 @@ class _CandleChartView extends StatelessWidget {
             activationMode: ActivationMode.singleTap,
           ),
         ));
+  }
+}
+
+class _Orderbook extends StatelessWidget {
+  const _Orderbook({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width / 2;
+    final titleStyle = Theme.of(context).textTheme.caption.copyWith(
+          color: AppColors.secondary,
+          fontSize: 12.0,
+        );
+    return Row(
+      children: [
+        Container(
+          padding: const EdgeInsets.only(
+            left: AppSizes.medium,
+            right: AppSizes.extraSmall / 2,
+          ),
+          width: width,
+          child: Column(
+            children: [
+              SizedBox(
+                height: AppSizes.extraLarge,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Volume', style: titleStyle),
+                    Text('Bid', style: titleStyle),
+                  ],
+                ),
+              ),
+              VolumeBidTile(volume: '1 566,28', bid: '0,94305', percent: 0.7),
+              VolumeBidTile(volume: '50 000,00', bid: '0,94305', percent: 0.99),
+              VolumeBidTile(volume: '93,09', bid: '0,94270', percent: 0.6),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+            ],
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.only(
+            right: AppSizes.medium,
+            left: AppSizes.extraSmall / 2,
+          ),
+          width: width,
+          child: Column(
+            children: [
+              SizedBox(
+                height: AppSizes.extraLarge,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Ask', style: titleStyle),
+                    Text('Volume', style: titleStyle),
+                  ],
+                ),
+              ),
+              VolumeAskTile(volume: '2,00', ask: '0,94606', percent: 0.35),
+              VolumeAskTile(volume: '50 000,00', ask: '0,94660', percent: 0.99),
+              VolumeAskTile(volume: '648,69', ask: '0,95867', percent: 0.7),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+              VolumeBidTile(volume: null, bid: null, percent: null),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Tradelog extends StatelessWidget {
+  const _Tradelog({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('tradelog'));
   }
 }
---
+++ lib/ui/widgets/volume_ask_tile.dart
@@ -0,0 +1,53 @@
+import 'package:antares_wallet/ui/common/app_colors.dart';
+import 'package:antares_wallet/ui/common/app_sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
+
+class VolumeAskTile extends StatelessWidget {
+  const VolumeAskTile({
+    @required this.volume,
+    @required this.ask,
+    @required this.percent,
+    Key key,
+  }) : super(key: key);
+
+  final String volume;
+  final String ask;
+  final double percent;
+
+  @override
+  Widget build(BuildContext context) {
+    final style = Theme.of(context).textTheme.button.copyWith(
+          fontWeight: FontWeight.w600,
+          fontSize: 12.0,
+        );
+    return Container(
+      height: AppSizes.extraLarge,
+      alignment: Alignment.center,
+      child: Stack(
+        fit: StackFit.expand,
+        alignment: Alignment.center,
+        children: [
+          LinearPercentIndicator(
+            lineHeight: AppSizes.extraLarge - 2,
+            padding: const EdgeInsets.all(0.0),
+            linearStrokeCap: LinearStrokeCap.butt,
+            percent: percent ?? 0,
+            backgroundColor: Colors.transparent,
+            progressColor: AppColors.red.withOpacity(0.1),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: AppSizes.extraSmall),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(ask ?? '—', style: style.apply(color: AppColors.red)),
+                Text(volume ?? '—', style: style),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
