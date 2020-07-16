77d4d0fd41d11a43e6e5e5914e1bdbc9c9d26a80
Antoraro <anton.rozdorozhnuk@gmail.com>
2020-07-16 16:23:18 +0300

    changed price update repeatable fetching to stream

--- lib/services/repositories/price_repository.dart
+++ lib/services/repositories/price_repository.dart
@@ -1,18 +1,32 @@
-import 'dart:async';
-
 import 'package:antares_wallet/services/api/api_service.dart';
 import 'package:antares_wallet/src/apiservice.pbgrpc.dart';
+import 'package:grpc/grpc.dart';
 
 class PriceRepository {
-  PricesResponse _currentPrices = PricesResponse.getDefault();
+  final ResponseStream<PriceUpdate> pricesStream =
+      ApiService.client.getPriceUpdates(PriceUpdatesRequest());
 
-  List<PriceUpdate> get prices => _currentPrices.prices;
+  List<PriceUpdate> _currentPrices = List();
+
+  List<PriceUpdate> get prices => _currentPrices;
 
   PriceRepository() {
-    Timer.periodic(const Duration(minutes: 1), (timer) => _fetchPrices());
+    _loadAllPrices();
+    pricesStream.listen((value) => _updatePrice(value));
   }
 
-  _fetchPrices() async {
-    _currentPrices = await ApiService.client.getPrices(PricesRequest());
+  _loadAllPrices() async {
+    _currentPrices =
+        (await ApiService.client.getPrices(PricesRequest())).prices;
+  }
+
+  _updatePrice(PriceUpdate priceUpdate) {
+    int index = _currentPrices.indexOf(priceUpdate);
+    print(priceUpdate);
+    if (index < 0) {
+      _currentPrices.add(priceUpdate);
+    } else {
+      _currentPrices[index] = priceUpdate;
+    }
   }
 }
