e0c6dd0a59cc8fe9609cd52b739333f5b6c4418d
Anton R <anton.rozdorozhnuk@gmail.com>
2021-01-20 18:46:38 +0200

    common.proto updated and generated

--- lib/app/data/grpc/common.pbenum.dart
+++ lib/app/data/grpc/common.pbenum.dart
@@ -12,6 +12,19 @@ import 'package:protobuf/protobuf.dart' as $pb;
 class ErrorCode extends $pb.ProtobufEnum {
   static const ErrorCode Unknown = ErrorCode._(0, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'Unknown');
   static const ErrorCode InvalidField = ErrorCode._(1, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'InvalidField');
+  static const ErrorCode MaintenanceMode = ErrorCode._(2, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'MaintenanceMode');
+  static const ErrorCode AssetUnavailable = ErrorCode._(3, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'AssetUnavailable');
+  static const ErrorCode PendingDialog = ErrorCode._(4, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'PendingDialog');
+  static const ErrorCode KycRequired = ErrorCode._(5, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'KycRequired');
+  static const ErrorCode NoData = ErrorCode._(6, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'NoData');
+  static const ErrorCode LimitationCheckFailed = ErrorCode._(7, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'LimitationCheckFailed');
+  static const ErrorCode AmountIsBiggerThanLimit = ErrorCode._(8, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'AmountIsBiggerThanLimit');
+  static const ErrorCode AmountIsBiggerThanLimitAndUpgrade = ErrorCode._(9, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'AmountIsBiggerThanLimitAndUpgrade');
+  static const ErrorCode InconsistentData = ErrorCode._(10, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'InconsistentData');
+  static const ErrorCode MinVolume = ErrorCode._(11, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'MinVolume');
+  static const ErrorCode InvalidCashoutAddress = ErrorCode._(12, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'InvalidCashoutAddress');
+  static const ErrorCode NotEnoughFunds = ErrorCode._(13, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'NotEnoughFunds');
+  static const ErrorCode BadRequest = ErrorCode._(400, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'BadRequest');
   static const ErrorCode Unauthorized = ErrorCode._(401, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'Unauthorized');
   static const ErrorCode NotFound = ErrorCode._(404, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'NotFound');
   static const ErrorCode Runtime = ErrorCode._(500, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'Runtime');
@@ -19,6 +32,19 @@ class ErrorCode extends $pb.ProtobufEnum {
   static const $core.List<ErrorCode> values = <ErrorCode> [
     Unknown,
     InvalidField,
+    MaintenanceMode,
+    AssetUnavailable,
+    PendingDialog,
+    KycRequired,
+    NoData,
+    LimitationCheckFailed,
+    AmountIsBiggerThanLimit,
+    AmountIsBiggerThanLimitAndUpgrade,
+    InconsistentData,
+    MinVolume,
+    InvalidCashoutAddress,
+    NotEnoughFunds,
+    BadRequest,
     Unauthorized,
     NotFound,
     Runtime,
